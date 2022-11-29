d159aac244be07f71d151e38aa7f1a86eb76a3cc
Mhinkyu <mhinkyuk@gmail.com>
2022-11-29 19:22:57 +0200

    Json output added, SSM password function

--- Lab-10/main.tf
+++ Lab-10/main.tf
@@ -20,46 +20,78 @@ resource "aws_db_instance" "prod_MK" {
     skip_final_snapshot = true
     apply_immediately = true
     username = "administrator"
-    password = random_password.main.result    
+    password = data.aws_secretsmanager_secret_version.rds_password.secret_string   
 }
 
+// Generate Password
 resource "random_password" "main" {
-    length = 20
-    special = true
-    override_special = "#!()@"
+  length           = 20
+  special          = true #   Default: !@#$%&*()-_=+[]{}<>:?
+  override_special = "#!()_"
 }
+
 // Store Password
 resource "aws_secretsmanager_secret" "rds_password" {
-    name = "/prod/rds/password"
-    description = "Password for my RDS Database"
-    recovery_window_in_days = 0
+  name                    = "/prod/rds/password"
+  description             = "Password for my RDS Database"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "rds_password" {
-    secret_id = aws_secretsmanager_secret.rds_password.id
-    secret_string = random_password.main.result
+  secret_id     = aws_secretsmanager_secret.rds_password.id
+  secret_string = random_password.main.result
 }
 
-// Retrive Password
+
+// Store All RDS parameters
+resource "aws_secretsmanager_secret" "rds" {
+  name                    = "/prod/rds/all"
+  description             = "All Details for my RDS Database"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "rds" {
+  secret_id = aws_secretsmanager_secret.rds.id
+  secret_string = jsonencode({
+    rds_address  = aws_db_instance.prod.address
+    rds_port     = aws_db_instance.prod.port
+    rds_username = aws_db_instance.prod.username
+    rds_password = random_password.main.result
+  })
+}
+
+
+// Retrieve Password
 data "aws_secretsmanager_secret_version" "rds_password" {
-    secret_id = aws_secretsmanager_secret.rds_password.id
-    
+  secret_id  = aws_secretsmanager_secret.rds_password.id
+  depends_on = [aws_secretsmanager_secret_version.rds_password]
 }
 
-#--------
+// Retreive ALL
+data "aws_secretsmanager_secret_version" "rds" {
+  secret_id  = aws_secretsmanager_secret.rds.id
+  depends_on = [aws_secretsmanager_secret_version.rds]
+}
+
+
+#-------
 output "rds_address" {
-    value = aws_db_instance.prod_MK.address
+  value = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["rds_address"]
 }
 
 output "rds_port" {
-    value = aws_db_instance.prod_MK.port
+  value = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["rds_port"]
 }
 
 output "rds_username" {
-    value = aws_db_instance.prod_MK.username
+  value = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["rds_username"]
 }
 
 output "rds_password" {
-    value = random_password.main.result
-    sensitive = true
+  value     = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["rds_password"]
+  sensitive = true
+}
+
+output "rds_all" {
+  value = nonsensitive(jsondecode(data.aws_secretsmanager_version.rds.secret_string))
 }
