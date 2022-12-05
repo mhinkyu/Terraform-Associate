f76bd13395c85b450b2251e1422999a1d9cc0866
Mhinkyu <mhinkyuk@gmail.com>
2022-12-05 11:54:53 +0200

    output files updtaed

---
+++ Lab-14/outputs.tf
@@ -0,0 +1,12 @@
+output "public_ip" {
+    value = aws.eip.web.public_ip  
+}
+
+output "server_id" {
+    value = aws_instance.web.id
+}
+
+output "securitygroup_id" {
+    value = aws_security_group.web.id
+}
+
