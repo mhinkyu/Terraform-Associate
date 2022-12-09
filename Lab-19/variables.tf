e5d0d19583ef557901f6a2b912ddf7b806a4fdf9
Mhinkyu <mhinkyuk@gmail.com>
2022-12-09 09:42:22 +0200

    file lab-19 added

---
+++ Lab-19/variables.tf
@@ -0,0 +1,30 @@
+variable "env" {
+    default = "test"
+}
+
+variable "server_size" {
+    default = {
+        prod = "t3.large"
+        staging = "t3.medium"
+        dev = "t3.small"
+        my_default = "t3.nano"
+    }
+}
+
+variable "ami_id_per_region" {
+    description = "My custom AMI id per region"
+    default = {
+    "us-west-2"  = "ami-0e472933a1395e172"
+    "us-west-1"  = "ami-08d9a394ac1c2994c"
+    "eu-west-1"  = "ami-0ce1e3f77cd41957e"
+    "ap-south-1" = "ami-08f63db601b82ff5f"
+    }
+}
+
+variable "allow-port" {
+    default = {
+        prod = ["80", "443"]
+        rest = ["80", "443", "8080", "22"]
+    }
+}
+
