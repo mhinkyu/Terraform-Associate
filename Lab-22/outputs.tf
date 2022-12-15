569dfc691018bee14fbf9e1bf05ad84334df58fa
Mhinkyu <mhinkyuk@gmail.com>
2022-12-15 15:25:00 +0200

    Loop Construct: for x in xyz

--- Lab-22/outputs.tf
+++ Lab-22/outputs.tf
@@ -3,7 +3,7 @@ output "instance_ids" {
 }
 
 output "instances_public_ips" {
-    value = aws_instances.my_server[*].public _ip
+    value = aws_instances.my_server[*].public_ip
 }
 
 output "server_id_ip" {
@@ -11,4 +11,27 @@ output "server_id_ip" {
         for x in aws_instance.my_server
         "Server with Id:
     }
+}
+
+
+output "server_id_ip_map" {
+  value = {
+        for x in aws_instance.my_server :
+        x.id => x.public
+    }
+}
+
+output "users_unique_id_arn" {
+  value = [
+    for user in aws_iam_user.user :
+    "UserID: ${user.unique_id} has ARN: ${user.arn}"
+  ]
+}
+
+output "users_unique_id_name_custom" {
+  value = {
+    for user in aws_iam_user.user :
+    user.unique_id => user.name // "AIDA4BML4S5345K74HQFF" : "john"
+    if length(user.name) < 7
+  }
 }
